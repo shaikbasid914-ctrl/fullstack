88b41184d4d68dca69301c5eb69aeccb8fd7e085
shaik <shaikbasid914@gmail.com>
2026-04-19 23:19:27 +0530

    Add files via upload

---
+++ MICROSERVICES/user_service/db_config.py
@@ -0,0 +1,9 @@
+import mysql.connector
+
+def get_db_connection():
+    return mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="Samuel@123",   
+        database="user_db"
+    )
