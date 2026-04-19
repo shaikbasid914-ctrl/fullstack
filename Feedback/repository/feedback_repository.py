dfc268b8e5a79a80a3794bee8176ced2fee3295b
shaik <shaikbasid914@gmail.com>
2026-04-19 23:18:45 +0530

    Add files via upload

---
+++ Feedback/repository/feedback_repository.py
@@ -0,0 +1,16 @@
+from db_config import get_db_connection
+
+def save_feedback(name, email, message):
+    conn = get_db_connection()
+    cursor = conn.cursor()
+
+    query = """
+        INSERT INTO feedback (name, email, message)
+        VALUES (%s, %s, %s)
+    """
+    cursor.execute(query, (name, email, message))
+    conn.commit()
+
+    cursor.close()
+    conn.close()
+
---
+++ Feedback/task4/config.py
@@ -0,0 +1,5 @@
+
+class Config:
+    SECRET_KEY = 'dev_secret_key'
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:Samuel@123!@localhost/student_db'
+SQLALCHEMY_TRACK_MODIFICATIONS = False
